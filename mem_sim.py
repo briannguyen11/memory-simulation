b2075d31fd26ee2257c4d39020fb8a8ed649ea3f
Brian Nguyen <nguyenbrian92620@gmail.com>
2023-11-04 15:04:44 -0700

    more modifications

--- mem_sim.py
+++ mem_sim.py
@@ -13,19 +13,33 @@ def get_logical_addresses(filename: str):
     return logical_addr_list
 
 
-def main():
-    tlb = {}
-    page_table = {}
-
-    addr_list = get_logical_addresses("addresses.txt")
-
-    for addr in addr_list:
-        print(f"Page Number {addr.page_num}, Offset {addr.offset} ")
+def find_frame_num(tlb, page_table, logical_addr_list):
     # once you have page number, look in tlb
     # if it is found in the tlb --> go straight to physical
     # if not found in tlb --> go to page table
     # if found in page talbe --> go straight to physical
     # if not found in page table --> pull from bin file and load into physical
+
+    for addr in logical_addr_list:
+        if addr in tlb:
+            print("found logical in tlb")
+        else:
+            print("going into page table")
+            if addr in page_table:
+                print("found logical in page table")
+            else:
+                print("pull from bin file")
+
+
+def main():
+    tlb = {}
+    page_table = {}
+
+    addr_list = get_logical_addresses("addresses.txt")
+    find_frame_num(tlb, page_table, addr_list)
+
+    for addr in addr_list:
+        print(f"Page Number {addr.page_num}, Offset {addr.offset} ")
 
 
 if __name__ == "__main__":
