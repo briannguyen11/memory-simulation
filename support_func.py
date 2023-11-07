513958652a7c63fdf30631311d0613fcd440bfe0
Brian Nguyen <nguyenbrian92620@gmail.com>
2023-11-07 12:18:36 -0800

    final

--- support_func.py
+++ support_func.py
@@ -6,23 +6,3 @@ class LogicalAddr:
         self.offset = offset
 
 
-class LRUCache:
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.phys_table = set()  # Stores the pages currently in physical memory
-        self.usage_order = []  # Maintains the order of page usage
-
-    def access_page(self, page):
-        if page in self.phys_table:
-            # Page is already in memory, move it to the most recently used position
-            self.usage_order.remove(page)
-            self.usage_order.append(page)
-        else:
-            # Page is not in memory, add it to memory
-            if len(self.phys_table) >= self.capacity:
-                # Memory is full, remove the least recently used page
-                lru_page = self.usage_order[0]
-                self.cache.remove(lru_page)
-                self.usage_order.pop(0)
-            self.cache.add(page)
-            self.usage_order.append(page)
